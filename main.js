65de3bb8f65dfb4da62992863df9445e3ccc1730
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2022-03-05 22:05:15 -0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 song = "";
+song2 = "";
+
+song1_status = "";
+song2_status = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -6,6 +14,13 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
 }
 
 function draw() {
@@ -15,9 +30,26 @@ function draw() {
 
 
 function preload() {
-    song = loadSound("music.mp3");
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
 }
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+
+    }
 }
