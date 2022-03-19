c68a88019fff3594c70e33404c0de167b365b7c6
idunnoiam <74386219+idunnoiam@users.noreply.github.com>
2022-03-18 21:25:46 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -25,12 +27,38 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
-    song.play();
+
+    song1_status = song.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song2.stop();
+
+        if (song1_status == false) {
+            song.play();
+            document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song";
+        }
+    }
+
+    if (scoreLeftWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song.stop();
+
+        if (song2_status == false) {
+            song2.play();
+            document.getElementById("song").innerHTML = "Playing - Peter Pan Song";
+        }
+    }
+
 }
 
 
 function preload() {
-    song1 = loadSound("music.mp3");
+    song = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
 }
 
@@ -43,6 +71,8 @@ function play() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
